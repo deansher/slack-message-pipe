d3df261a815203c09360ba17e1c0d310a723ffc0
deansher <deansherthompson@gmail.com>
2024-02-06 18:23:19 -0500

    updated docstring and __version__ in root __init__.py

--- slack_message_pipe/__init__.py
+++ slack_message_pipe/__init__.py
@@ -1,7 +1,8 @@
-"""Command line tool for exporting the text contents of any Slack channel to a PDF file."""
+"""a library for reading a Slack channel's message history as simple Python data structures"""
 
 # MIT License
 #
 # Copyright (c) 2019 Erik Kalkoken
+# Copyright (c) 2024 Dean Thompson
 
-__version__ = "2.0.0"
+__version__ = "0.0.4"
